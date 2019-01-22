bc2cfbc30ec7f6c21e95fe9b95e9c4c1a90ca6fe
peter-jhgao <29678838@qq.com>
2019-01-22 11:26:37 +0800

    Update HelloWorldController.java

--- src/main/java/com/example/springboot/web/HelloWorldController.java
+++ src/main/java/com/example/springboot/web/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping("/")
+    @RequestMapping("/spring")
     public String sayHello() {
         return "Hello CCE from CSE!";
     }
